5882f4def40e823006f9a334000eb3d8a485cb14
Thailan <thailanmendes88@gmail.com>
2022-05-06 20:13:58 -0300

    ex novos

---
+++ exercicios/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`a velocidade do seu carro é ${vel} Km/h`)
+if (vel > 60) {  //condicao simples!
+    console.log(`Vc ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de seguranca`)
