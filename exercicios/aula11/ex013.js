44f2e22c2893ddbe2b64d8f0a77f1048d486ca0a
Thailan <thailanmendes88@gmail.com>
2022-05-10 16:19:49 -0300

    exercicios novos

---
+++ exercicios/aula11/ex013.js
@@ -0,0 +1,14 @@
+var agora = new Date() // pegar a hora exata do sistema
+var hora = agora.getHours()
+console.log(`Agora sao exatamente ${hora} horas`)
+if (hora <= 12) {
+    console.log('Bom dia')
+}else if (hora <= 18) {
+    console.log('Boa tarde')
+}else {
+    console.log('Boa noite')
+}
+
+
+
+
