437a462e50175f53b8a34bddcf54c995ab51b461
Thailan <thailanmendes88@gmail.com>
2022-05-18 20:09:32 -0300

    ex novos

---
+++ exercicios/aula14/ambiente.js
@@ -0,0 +1,28 @@
+var c = 1
+do {  //do = 'faca'
+    console.log(`Passo ${c}`)
+    c ++ // c = c + 1
+} while (c <= 6)  //while = 'enquanto'
+
+
+for (var c = 1; c <= 6; c++) {
+    console.log(`Passo ${c}`)
+}
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)    //jeito mais encontrado dentro dos programas
+    c++ // c = c + 1
+}*/
+
+
+
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')  // pior maneira 
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
