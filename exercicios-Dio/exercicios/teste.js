faa52ecd3a7e4f1bf61bd67ff6215c2ed2b039ca
Thailan <thailanmendes88@gmail.com>
2022-06-03 15:14:11 -0300

    att

--- exercicios-Dio/exercicios/teste.js
+++ exercicios-Dio/exercicios/teste.js
@@ -4,3 +4,20 @@ function soma(a=3,b=8 ) {
 console.log(soma())
 
 console.log(`a soma dos valores sao ${soma()}`)
+
+
+
+
+var firstname = 'Thailan';
+var lastname = 'Mends';
+var idade = 15
+
+if(firstname  === 'Thailan' && idade >= 15){
+    var idade = 15
+    console.log (`seu primeiro nome é ${firstname} e vc tem ${idade} anos`)
+} else if( lastname === 'Mendes'){
+    console.log (`Seu sobrenome é ${lastname}`)
+} else {
+    console.log ('vc nao tem nome foi adotado')
+}
+
