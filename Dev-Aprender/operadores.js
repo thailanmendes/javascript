1d0a9f4ec9bf79836d4b2e9f765ea2e437f370d2
Thailan <thailanmendes88@gmail.com>
2022-06-10 13:34:27 -0300

    exercicios Dev-Aprender

---
+++ Dev-Aprender/operadores.js
@@ -0,0 +1,9 @@
+//Operadores aritméticos(matemáticos)
+//Operadores de atribuição
+//Operadores de comparação
+//Operadores Lógicos 
+//Operadores Bitwise
+
+
+//Aritméticos  +, - , * , / , e o ** =  exponencial, um numero vezes ele mesmo
+let salario = 100;
