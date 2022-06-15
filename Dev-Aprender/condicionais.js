fffbb05161e0ae501c86761e3c9ccf32f3524d95
Thailan <thailanmendes88@gmail.com>
2022-06-15 09:44:15 -0300

    switch case

--- Dev-Aprender/condicionais.js
+++ Dev-Aprender/condicionais.js
@@ -14,3 +14,27 @@ if(hora > 6 && hora < 12 ){
 }else{
     console.log('Boa noite!')
 }
+
+
+
+//switch case
+
+let permissao; // comum, gerente, diretor
+permissao = 'gerente';
+
+switch (permissao){
+    case 'comum':
+    console.log('usuario comum');
+    break;
+
+    case 'gerente':
+    console.log('usuario gerente');
+    break;
+
+    case 'diretor':
+    console.log('usuario diretor');
+    break;
+
+    default:
+    console.log ('usuario nao reconhecido!');
+}
