1ee08faef27f3e33453b7c2818d9825d84588228
Thailan <thailanmendes88@gmail.com>
2022-06-15 17:29:19 -0300

    att

--- Dev-Aprender/Loop-for-in-for-off.js
+++ Dev-Aprender/Loop-for-in-for-off.js
@@ -7,14 +7,14 @@ const pessoa = {
     idade: 29
 };
 
-for(let chave in pessoa){
+for(let chave in pessoa){  //chave = key-value    name= key(chave), Thailan = value(valor)
     console.log(chave,pessoa[chave]);
 }
 
 
 const cores = ['red', 'blue', 'black'];
 
-for (let indice in cores){
+for (let indice in cores){ //in = buscar informação 
     console.log(indice,cores[indice])
 }
 
