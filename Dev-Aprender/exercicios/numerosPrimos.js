cadbd37b23cbc479bb955ada174633c35c32922c
Thailan <thailanmendes88@gmail.com>
2022-06-20 16:03:51 -0300

    numerosPrimos

---
+++ Dev-Aprender/exercicios/numerosPrimos.js
@@ -0,0 +1,21 @@
+//Criar funcao para mostrar os números primos
+
+//Primos
+//Compostor
+
+exibirNumerosPrimos(15)
+
+function exibirNumerosPrimos(limite){
+    for(let numero = 2; numero <= limite; numero++){
+        let ehPrimo = true;
+
+        for(let divisor = 2; divisor < numero; divisor++){
+            if(numero % divisor === 0) {
+                ehPrimo = false;
+                break;
+            }
+        }
+
+        if(ehPrimo) console.log(numero)
+    }
+}
