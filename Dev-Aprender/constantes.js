37f1cdd957965860430622619c9e58205cba76d2
Thailan <thailanmendes88@gmail.com>
2022-06-22 11:06:36 -0300

    add pasta

--- Dev-Aprender/constantes.js
+++
@@ -1,9 +0,0 @@
-let valorIngressoAdulto = 30;
-valorIngressoAdulto = 40;
-console.log(valorIngressoAdulto);
-//let é bastante usado para alterar valores 
-
-
-//const é usado pra valores q nao podem ser definidos 
-const igual = 50;
-console.log(igual)
--- Dev-Aprender/tiposPrimitivos.js
+++
@@ -1,7 +0,0 @@
-let name = 'Thailan'; //string literal
-let idade = 29 ; //number literal
-let estaAprovado = true; //boolean
-let sobrenome = undefined; // Undefined
-let corSelecionada = null // para redefinir um valor
-
-
