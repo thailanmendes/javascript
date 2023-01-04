2c319783be116ea64f3b0e68806d0ecff581d2e7
Thailan <thailanmendes88@gmail.com>
2023-01-04 16:24:50 -0300

    add comentarios

--- Curso Web JavaScript-Udemy/fundamentos/arrays.js
+++ Curso Web JavaScript-Udemy/fundamentos/arrays.js
@@ -1,16 +1,16 @@
 const valores =  [7.7, 8.9,  6.3,  9.2]
-console.log (valores[0],valores[ 3 ])
-console.log (valores[4])
+console.log (valores[0],valores[3])
+console.log (valores[4])  //undefined
 
 valores[4]  =  10
 console.log( valores )
-console.log(valores . length )
+console.log(valores.length )
 
-valores.push( { id: 3 } ,  false ,  null ,  'teste' )
+valores.push( { id: 3 } ,  false ,  null ,  'teste' ) // push - add elementos no array
 console.log ( valores )
 
-console.log(valores.pop())
-delete valores [0]
+console.log(valores.pop()) // pop - elimina o ultimo elemento do array
+delete valores [0] //eliminar elementos do array
 console.log (valores)
 
-console.log(typeof valores)
+console.log(typeof valores)  //typeof, ver o tipo
