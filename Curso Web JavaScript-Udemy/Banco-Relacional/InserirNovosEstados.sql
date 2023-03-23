71c762eb21780fe479e0c3021ac6bfd870e9e246
Thailan <thailanmendes88@gmail.com>
2023-03-23 00:05:29 -0300

    add commits

--- Curso Web JavaScript-Udemy/Banco-Relacional/InserirNovosEstados.sql
+++ Curso Web JavaScript-Udemy/Banco-Relacional/InserirNovosEstados.sql
@@ -3,7 +3,7 @@ value (1000, 'Novo', 'NV', 'Sul', 2.54)
 
 select * from estados
 
-
+--caso nao coloque o id, o proximo elemento é inserido a partir do ultimo elemento !
 insert into estados (nome, sigla, regiao, populacao)
 value ('Mais Novo', 'MN', 'Norte', 2.51)
 
