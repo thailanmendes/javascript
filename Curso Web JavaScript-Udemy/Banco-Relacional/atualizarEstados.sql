dc95a842e8ae8007ac91e9ecad71fe1f07746043
Thailan <thailanmendes88@gmail.com>
2023-03-22 23:40:53 -0300

    comentando updates

--- Curso Web JavaScript-Udemy/Banco-Relacional/atualizarEstados.sql
+++ Curso Web JavaScript-Udemy/Banco-Relacional/atualizarEstados.sql
@@ -1,3 +1,4 @@
+--colocando o ~ no maranhão!
 UPDATE estados
 set nome = 'Maranhão'
 where sigla = 'MA'
@@ -5,13 +6,14 @@ where sigla = 'MA'
 
 select nome from estados where sigla = 'MA'
 
+--atualizando o nome do estado acrescentando a virgula!
 update estados set nome = 'Paraná',
 populacao = 11.32
 where sigla = 'PR'
 
 select est.nome, sigla, populacao from estados est where sigla = 'PR'
 
-
+--atualizando populacao da paraiba!
 update estados 
 set populacao = '15.22'
 where sigla = 'PB'
