18386e43c9d8d74866575e8f2acf2e48194b453b
Thailan <thailanmendes88@gmail.com>
2023-03-23 00:10:45 -0300

    deletando estados

---
+++ Curso Web JavaScript-Udemy/Banco-Relacional/excluirestados.sql
@@ -0,0 +1,11 @@
+--deletando algo do banco de dados, nesse caso estou deletando com referecia da sigla 'MO' !
+DELETE from `estados`
+where sigla = 'MO'
+
+
+select * from `estados`
+
+--deletando id >= 1000 !
+delete from `estados`
+where id >= 1000
+
